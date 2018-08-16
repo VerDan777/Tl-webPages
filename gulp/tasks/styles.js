eb0147e170b4d709010733ad754f4a78fbda7f80
verdan777 <vereshhagin98@mail.ru>
2018-08-16 23:23:55 +0300

    new

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,19 +1,20 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const SassModuleImporter = require("sass-module-importer");
 const notify = require("gulp-notify");
 const autoprefixer = require("gulp-autoprefixer");
-const SassModuleImporter = require("sass-module-importer");
 
 gulp.task("styles", () => {
     return gulp.src("./src/scss/**/*.scss")
-    .pipe(sass({importer: SassModuleImporter()}))
-    .on("error", notify.onError(function(error){
-        console.log(error.toString());
-        this.emit("end");
-    }))
-    .pipe(autoprefixer({
-        browsers: ["last 2 versions"],
-        cascade: false
-    }))
-    .pipe(gulp.dest("./dist"))
-})
+        .pipe(sass({importer: SassModuleImporter()}))
+        .on("error", notify.onError(function(error) {
+            return {
+                title: "Styles",
+                message: error.message
+            };
+        }))
+        .pipe(autoprefixer({
+            browsers: "last 2 versions"
+        }))
+        .pipe(gulp.dest("./dist"));
+});
