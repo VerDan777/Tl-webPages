4c53cd3edb8a96fe9a5fb06981e7c649fbab161c
verdan777 <vereshhagin98@mail.ru>
2018-08-21 09:10:10 +0300

    mobile version

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const watch = require("gulp-watch");
 const browserSync = require("browser-sync").create();
 
-gulp.task("watch", function() {
+gulp.task("watch", () => {
     browserSync.init({
         server: {
             baseDir: "dist"
@@ -10,37 +10,45 @@ gulp.task("watch", function() {
         notify: {
             styles: {
                 top: "auto",
-                bottom: 0
+                bottom: "0"
             }
         }
     });
 
-    // pug
-    watch("./src/**/*.pug", function() {
+    watch("./src/**/*.pug", () => {
         gulp.start("pugChanged");
     });
 
-    // css
-    watch("./src/scss/**/*.scss", function() {
+    watch("./src/scss/**/*.scss", () => {
         gulp.start("cssInject");
     });
 
-    // js
-    watch("./src/js/**/*.js", function() {
+    watch("./src/js/**/*.js", () => {
         gulp.start("jsChanged");
     });
 
+    // watch("./wp/*.php", () => {
+    //     gulp.start("copyPHP");
+    // });
+
+    // watch("./dist/styles.css", () => {
+    //     gulp.start("copyCSS");
+    // });
+
+    // watch("./dist/*.js", () => {
+    //     gulp.start("copyJS");
+    // });
 });
 
-gulp.task("pugChanged", ["pugRender"], function() {
+gulp.task("pugChanged", ["pugRender"], () => {
     browserSync.reload();
 });
 
-gulp.task("cssInject", ["styles"], function() {
+gulp.task("cssInject", ["styles"], () => {
     gulp.src("./dist/styles.css")
         .pipe(browserSync.stream());
 });
 
-gulp.task("jsChanged", ["scripts"], function() {
+gulp.task("jsChanged", ["scripts"], () => {
     browserSync.reload();
 });
